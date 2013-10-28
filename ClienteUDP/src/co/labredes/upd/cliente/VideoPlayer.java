42b655508b526fdd2dbdcab9661da40a9bf59d05
Santiago Rojas <sajhu93@gmail.com>
2013-10-27 21:30:22 -0500

    intento xuggler

--- ClienteUDP/src/co/labredes/upd/cliente/VideoPlayer.java
+++ ClienteUDP/src/co/labredes/upd/cliente/VideoPlayer.java
@@ -1,116 +1,84 @@
 package co.labredes.upd.cliente;
 
-import javax.swing.*;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IStream;
+import com.xuggle.xuggler.IStreamCoder;
 
-import javafx.application.Platform;
-import javafx.beans.value.*;
-import javafx.embed.swing.JFXPanel;
-import javafx.event.*;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
-import javafx.scene.media.*;
-import javafx.util.Duration;
+public class VideoPlayer {
+    
+    private static final String filename = ".data/Wildlife.mp4";
+    
+    public static void main(String[] args) {
+        
+        // first we create a Xuggler container object
+        IContainer container = IContainer.make();
+        
+        // we attempt to open up the container
+        int result = container.open(filename, IContainer.Type.READ, null);
+        
+        // check if the operation was successful
+        if (result<0)
+            throw new RuntimeException("Failed to open media file");
+        
+        // query how many streams the call to open found
+        int numStreams = container.getNumStreams();
+        
+        // query for the total duration
+        long duration = container.getDuration();
+        
+        // query for the file size
+        long fileSize = container.getFileSize();
 
-public class VideoPlayer extends JFrame {
+        // query for the bit rate
+        long bitRate = container.getBitRate();
+        
+        System.out.println("Number of streams: " + numStreams);
+        System.out.println("Duration (ms): " + duration);
+        System.out.println("File Size (bytes): " + fileSize);
+        System.out.println("Bit Rate: " + bitRate);
+        
+        // iterate through the streams to print their meta data
+        for (int i=0; i<numStreams; i++) {
+            
+            // find the stream object
+            IStream stream = container.getStream(i);
+            
+            // get the pre-configured decoder that can decode this stream;
+            IStreamCoder coder = stream.getStreamCoder();
+            
+            System.out.println("*** Start of Stream Info ***");
+            
+            System.out.printf("stream %d: ", i);
+            System.out.printf("type: %s; ", coder.getCodecType());
+            System.out.printf("codec: %s; ", coder.getCodecID());
+            System.out.printf("duration: %s; ", stream.getDuration());
+            System.out.printf("start time: %s; ", container.getStartTime());
+            System.out.printf("timebase: %d/%d; ",
+                 stream.getTimeBase().getNumerator(),
+                 stream.getTimeBase().getDenominator());
+            System.out.printf("coder tb: %d/%d; ",
+                 coder.getTimeBase().getNumerator(),
+                 coder.getTimeBase().getDenominator());
+            System.out.println();
+            
+            if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO) {
+                System.out.printf("sample rate: %d; ", coder.getSampleRate());
+                System.out.printf("channels: %d; ", coder.getChannels());
+                System.out.printf("format: %s", coder.getSampleFormat());
+            } 
+            else if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
+                System.out.printf("width: %d; ", coder.getWidth());
+                System.out.printf("height: %d; ", coder.getHeight());
+                System.out.printf("format: %s; ", coder.getPixelType());
+                System.out.printf("frame-rate: %5.2f; ", coder.getFrameRate().getDouble());
+            }
+            
+            System.out.println();
+            System.out.println("*** End of Stream Info ***");
+            
+        }
+        
+    }
 
-	public static final String VID_URL = 
-			"http://static.clipcanvas.com/sample/clipcanvas_14348_H264_320x180.mp4";
-
-	private static final int VID_WIDTH     = 320;
-	private static final int VID_HEIGHT    = 180;
-	private static final int PLAYER_WIDTH  = 320;
-	private static final int PLAYER_HEIGHT = 265;
-
-	private void play(final String url) {
-		final JFXPanel panel = new JFXPanel();
-		Platform.runLater(new Runnable() {
-			@Override public void run() {
-				initFX(panel, url);
-			}
-		});
-		this.add(panel);
-		this.setSize(PLAYER_WIDTH, PLAYER_HEIGHT);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setVisible(true);
-	}
-
-	private void initFX(JFXPanel panel, String url) {
-		MediaView mediaView = createMediaView(url);
-
-		final Scene playerScene = new Scene(
-				createPlayerLayout(mediaView), 
-				PLAYER_WIDTH, 
-				PLAYER_HEIGHT
-				);	
-
-		setMediaEventHandlers(
-				mediaView
-				);
-
-		panel.setScene(playerScene);
-	}
-
-	private MediaView createMediaView(String url) {
-		final Media clip = new Media(url);
-		final MediaPlayer player = new MediaPlayer(clip);
-		final MediaView view = new MediaView(player);
-		view.setFitWidth(VID_WIDTH);
-		view.setFitHeight(VID_HEIGHT);
-		return view;
-	}
-
-	private VBox createPlayerLayout(final MediaView view) {
-		final Button button = new Button("Play From Start");
-		button.setOnAction(new EventHandler<ActionEvent>() {
-			@Override public void handle(ActionEvent event) {
-				view.getMediaPlayer().seek(Duration.ZERO);
-				view.getMediaPlayer().play();
-			}
-		});
-
-		final VBox layout = new VBox(8);
-		layout.setAlignment(Pos.CENTER);
-		layout.getChildren().addAll(
-				view,
-				button
-				);
-
-		layout.setStyle("-fx-background-color: linear-gradient(to bottom, derive(lightseagreen, -20%), lightseagreen);");
-
-		return layout;
-	}
-
-	private void setMediaEventHandlers(final MediaView view) {
-		final MediaPlayer player = view.getMediaPlayer();
-
-		System.out.println("Initial: " + player.getStatus());
-		player.statusProperty().addListener(new ChangeListener<MediaPlayer.Status>() {
-			@Override
-			public void changed(ObservableValue<? extends MediaPlayer.Status> observable, MediaPlayer.Status oldStatus, MediaPlayer.Status curStatus) {
-				System.out.println("Current: " + curStatus);
-			}
-		});
-
-		if (player.getError() != null) {
-			System.out.println("Initial Error: " + player.getError());
-		}
-
-		player.setOnError(new Runnable() {
-			@Override public void run() {
-				System.out.println("Current Error: " + player.getError());
-			}
-		});
-	}
-
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override public void run() {
-				VideoPlayer player = new VideoPlayer();
-				player.play(VID_URL);
-			}
-		});
-	}
 }
